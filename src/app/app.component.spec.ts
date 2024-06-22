9cc43fc01efccffaf3b91cd6a3a42ed436cbe7fb
sour9838 <ashleecast98@gmail.com>
2024-06-22 14:09:09 -0500

    1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, T3');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Holii, T3');
   });
 });
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
-}
+  login(usuario:string, password:string):void{
+    sessionStorage.setItem("isLogget",'true');  }
+    logout():void{
+      sessionStorage.clear();
+    }
+    isLogget(): boolean{
+      return !!sessionStorage.getItem("isLogget");
+    }
+  }
